eb5cc19007d39c7eefaba4112bcfa4106454e6a2
ezgiakardere <ezgiakardere35@gmail.com>
2023-07-10 22:44:13 +0300

    Proje dosyası ekle.

---
+++ Giriş projesi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giriş_projesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**HELLO GITHUB**");
+            Console.ReadKey();
+        }
+    }
+}
